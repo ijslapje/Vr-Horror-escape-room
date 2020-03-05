d39a900eb328481720ef233d18fec70b1effe977
Ian <ijslapje@gmail.com>
2020-03-05 11:46:04 +0100

    Added NON VR option

---
+++ Vr Game Practice/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject VRcam;
+    public GameObject mouseCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("1key")){
+            VRcam.SetActive(true);
+            mouseCam.SetActive(false);
+        }
+        else if(Input.GetButtonDown("2key")){
+            VRcam.SetActive(false);
+            mouseCam.SetActive(true);
+        }
+    }
+}
---
+++ Vr Game Practice/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 5.0f;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+    }
+
+}
