4ee4779b794d13753e8b01b2554ec2d8fa8744ce
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-03-17 14:59:08 +0100

    Yeet

---
+++ Vr Game Practice/Assets/Scripts/DrawerRestraints.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerRestraints : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //dit is voor debuggen. -----------------------------------|
+        if(Input.GetKeyDown(KeyCode.I)){
+            this.transform.Translate(new Vector3(0.1f, 0f, 0f));
+        }
+        else if(Input.GetKeyDown(KeyCode.K)){
+            this.transform.Translate(new Vector3(-0.1f, 0f, 0f));
+        }
+        //DUS VERWIJDER HET ----------------------------------------|
+
+        //dit is nogal gehacked. 
+        var pos = this.transform.localPosition;
+        pos.x =  Mathf.Clamp(this.transform.localPosition.x, this.transform.position.x-1, (this.transform.position.x-1) + this.transform.localScale.x / 2);
+        this.transform.localPosition = pos;
+    }
+}
