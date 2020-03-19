c4a0ec4749f56690e45f8e7355e56c415902bfd3
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-03-19 12:15:41 +0100

    Added DrawerLimit

--- Vr Game Practice/Assets/Scripts/DrawerLimit.cs
+++ Vr Game Practice/Assets/Scripts/DrawerLimit.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class DrawerLimit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+ // Update is called once per frame
     void Update()
     {
-        
+        //dit is voor debuggen. -----------------------------------|
+        if(Input.GetKeyDown(KeyCode.I)){
+            this.transform.Translate(new Vector3(0.1f, 0f, 0f));
+        }
+        else if(Input.GetKeyDown(KeyCode.K)){
+            this.transform.Translate(new Vector3(-0.1f, 0f, 0f));
+        }
+        //DUS VERWIJDER HET ----------------------------------------|
+
+        //dit is nogal gehacked. 
+        var pos = this.transform.localPosition;
+        pos.x =  Mathf.Clamp(this.transform.localPosition.x, this.transform.position.x-1, (this.transform.position.x-1) + this.transform.localScale.x / 2);
+        this.transform.localPosition = pos;
     }
 }
