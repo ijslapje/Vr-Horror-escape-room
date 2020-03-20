89d9e8a0901441928c271a857b36b2381daac5df
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-03-20 12:37:24 +0100

    Box Colliders Gefixt

--- Vr Game Practice/Assets/Scripts/DrawerLimit.cs
+++ Vr Game Practice/Assets/Scripts/DrawerLimit.cs
@@ -4,35 +4,23 @@ using UnityEngine;
 
 public class DrawerLimit : MonoBehaviour
 {
-
-    // Update is called once per frame
-    private void Start()
-    {
-
-    }
-
     void Update()
     {
-        //dit is voor debuggen. -----------------------------------|
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            this.transform.Translate(new Vector3(0.1f, 0f, 0f));
-        }
-        else if (Input.GetKeyDown(KeyCode.K))
-        {
-            this.transform.Translate(new Vector3(-0.1f, 0f, 0f));
-        }
-        //DUS VERWIJDER HET ----------------------------------------|
+        ////dit is voor debuggen. -----------------------------------|
+        //if (Input.GetKeyDown(KeyCode.I))
+        //{
+        //    this.transform.Translate(new Vector3(0.1f, 0f, 0f));
+        //}
+        //else if (Input.GetKeyDown(KeyCode.K))
+        //{
+        //    this.transform.Translate(new Vector3(-0.1f, 0f, 0f));
+        //}
+        ////pp poopoo -----------------------------------------------|
 
 
-        ////dit is nogal gehacked. 
-        //var pos = this.transform.localPosition;
-        //pos.x = Mathf.Clamp(this.transform.localPosition.x, this.transform.position.x - 1, (this.transform.position.x - 1) + this.transform.localScale.x / 2);
-        //this.transform.localPosition = pos;
         this.transform.rotation = Quaternion.Euler(-90f, 0f, 0f);
 
         DrawerSpace();
-
 
     }
 
