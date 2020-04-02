a64383f4dc521fe9a85e4cb8aac9625b66f37447
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-04-02 13:07:27 +0200

    Deleted door and Lock function in Test room

---
+++ Tarnished/Tarnished/Assets/Scripts/OnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+    public Collider coll;
+
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+
+    // Disables gravity on all rigidbodies entering this collider.
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody)
+            other.attachedRigidbody.useGravity = true;
+            other.attachedRigidbody.isKinematic = false;
+    }
+}
