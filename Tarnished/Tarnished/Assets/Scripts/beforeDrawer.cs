52ed7a9b2bc1ff2ab9fea06360765fa4ce09b999
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-04-03 10:57:05 +0200

    Drawer is locked, key to open it. Door schuif ook gwn fixen JONGEEEE LETS GOOOOO

---
+++ Tarnished/Tarnished/Assets/Scripts/beforeDrawer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beforeDrawer : MonoBehaviour
+{
+    public GameObject drawer;
+    public GameObject clone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OnCollision.opslot == false)
+        {
+            drawer.transform.gameObject.SetActive(true);
+            clone.transform.gameObject.SetActive(false);
+        }
+    }
+}
