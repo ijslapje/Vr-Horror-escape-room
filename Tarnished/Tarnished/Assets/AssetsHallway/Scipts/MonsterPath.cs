bbdea6f1d51eb3225fc579684ccb9b12f17d0139
Swen <TheNeonfox2000@gmail.com>
2020-04-03 15:30:12 +0200

    Monster Path finding + Door Physics

---
+++ Tarnished/Tarnished/Assets/AssetsHallway/Scipts/MonsterPath.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class MonsterPath : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject door;
+
+    public Transform[] target;
+    public float speed;
+
+    private bool walking;
+    private int current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        walking = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(walking == true)
+        {
+            if (transform.position != target[current].position)
+            {
+                Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+                anim.SetBool("walking", true);
+                GetComponent<Rigidbody>().MovePosition(pos);
+            }
+            else current = (current + 1) % target.Length;
+        }
+
+        if(current == 4)
+        {
+            walking = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "door")
+        {
+            door.GetComponent<Animator>().SetTrigger("Open");
+        }
+    }
+
+}
