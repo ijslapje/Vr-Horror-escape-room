4beb81014494b19dee9012c7e5e8b60da2adad85
Swen <TheNeonfox2000@gmail.com>
2020-04-16 15:14:58 +0200

    Fixed Monster + Added Sounds

--- Tarnished/Tarnished/Assets/AssetsHallway/Scipts/MonsterPath.cs
+++ Tarnished/Tarnished/Assets/AssetsHallway/Scipts/MonsterPath.cs
@@ -6,6 +6,7 @@ using UnityEngine.Animations;
 public class MonsterPath : MonoBehaviour
 {
     public Animator anim;
+    public AudioSource source;
 
     public Transform[] target;
     public float speed;
@@ -22,6 +23,7 @@ public class MonsterPath : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
         walking = true;
     }
 
@@ -44,23 +46,27 @@ public class MonsterPath : MonoBehaviour
             {
                 Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
                 GetComponent<Rigidbody>().MovePosition(pos);
+                source.loop = true;
                 if (current == 2)
                 {
                     anim.SetBool("walking", false);
+                    source.volume = 0f;
                     speed = 0.1f;
                 }
-                else if (current == 3)
+                else if (current != 2)
                 {
                     anim.SetBool("walking", true);
+                    source.volume = 0.7f;
                     speed = 1f;
                 }
             }
             else current = (current + 1) % target.Length;
         }
 
-        if (current == 4)
+        if (current > 4)
         {
             walking = false;
+            source.Stop();
         }
     }
 
