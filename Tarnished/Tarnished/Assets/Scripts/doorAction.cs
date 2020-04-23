a155d79adebc9bdd9c2b05ae082d64843b9d048c
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-04-23 14:47:03 +0200

    Door become hidge when vent pickuit

---
+++ Tarnished/Tarnished/Assets/Scripts/doorAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorAction : MonoBehaviour
+{
+    public GameObject doorclone;
+    public GameObject door;
+
+    void PickVentUp()
+    {
+        doorclone.transform.gameObject.SetActive(true);
+        door.transform.gameObject.SetActive(false);
+    }
+}
