b4c58006358ef412950a412ca620e83a252b4169
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-04-23 14:48:36 +0200

    Colliders bed also turn off

--- Tarnished/Tarnished/Assets/Scripts/doorAction.cs
+++ Tarnished/Tarnished/Assets/Scripts/doorAction.cs
@@ -6,10 +6,11 @@ public class doorAction : MonoBehaviour
 {
     public GameObject doorclone;
     public GameObject door;
-
+    public GameObject bedCollider;
     void PickVentUp()
     {
         doorclone.transform.gameObject.SetActive(true);
         door.transform.gameObject.SetActive(false);
+        bedCollider.transform.gameObject.SetActive(false);
     }
 }
