cf06b9b47184f51edc293137089b71d619e24a75
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-04-23 14:49:49 +0200

    Ok now it works the door open

--- Tarnished/Tarnished/Assets/Scripts/doorAction.cs
+++ Tarnished/Tarnished/Assets/Scripts/doorAction.cs
@@ -7,7 +7,7 @@ public class doorAction : MonoBehaviour
     public GameObject doorclone;
     public GameObject door;
     public GameObject bedCollider;
-    void PickVentUp()
+    public void PickVentUp()
     {
         doorclone.transform.gameObject.SetActive(true);
         door.transform.gameObject.SetActive(false);
